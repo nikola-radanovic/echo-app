9f99ea58704d5a08c8a96f28f213b4fdbc224718
nikola-radanovic <nikola.radanovic@everlyhealth.com>
2025-05-22 20:14:30 -0500

    Update firebaseConfig.js
    delete secret google cloud API key

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,7 +4,7 @@ import 'firebase/compat/auth';
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZ8oZ5pQhkGrVOkDPDdG6JLnid228VQBo",
+  apiKey: "",
   authDomain: "echoapp-1a8c7.firebaseapp.com",
   projectId: "echoapp-1a8c7",
   storageBucket: "echoapp-1a8c7.appspot.com",
